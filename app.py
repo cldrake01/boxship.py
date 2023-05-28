1ffc7448b48db35a4ad4c91891e5c22710247fa4
Collin Drake <collinlindendrake@gmail.com>
2023-05-28 12:31:48 -0600

    //

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import HTTPException
 from werkzeug.exceptions import default_exceptions
+from endpoints.users.resource import UsersResource
+from endpoints.todos.resource import TodosResource
 import settings
 
 app = Flask(__name__)
@@ -15,9 +17,9 @@ def handle_error(e):
         code = e.code
     return jsonify(error=str(e)), code
 
+
 for ex in default_exceptions:
     app.register_error_handler(ex, handle_error)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
@@ -26,9 +28,6 @@ app.config['BUNDLE_ERRORS'] = settings.BUNDLE_ERRORS
 db = SQLAlchemy(app)
 api = Api(app)
 api.prefix = '/api'
-
-from endpoints.users.resource import UsersResource
-from endpoints.todos.resource import TodosResource
 
 api.add_resource(UsersResource, '/users', '/users/<int:user_id>')
 api.add_resource(TodosResource, '/todos', '/todos/<int:todo_id>')
---
+++ functions/expose.py
@@ -0,0 +1,63 @@
+import json
+import functools
+import threading
+import asyncio
+from http.server import HTTPServer
+from typing import Any, Callable
+
+from aiohttp import web
+
+from flask import Flask, jsonify, request
+
+
+def expose(func: Callable[..., Any], methods: [str], port: int = 8080, hostname: str = "localhost",
+           route: str = "/") -> Callable[..., Any]:
+    """
+    Decorator to expose a function as an HTTP endpoint.
+
+    Args:
+        func: The function to be exposed.
+        methods: A list of HTTP methods allowed for accessing the endpoint.
+        port: The port number on which the server will listen (default: 8080).
+        hostname: The hostname or IP address on which the server will run (default: "localhost").
+        route: The URL route at which the endpoint will be accessible (default: "/").
+
+    Returns:
+        The decorated function that acts as the endpoint.
+
+    Notes:
+        This decorator creates a Flask application and configures a route for the provided function,
+        making it accessible via HTTP requests. The decorated function is wrapped to handle incoming
+        requests and call the original function with the parsed JSON arguments.
+
+        The web server is run in a separate thread, allowing it to be non-blocking and run concurrently
+        with the rest of the code.
+
+        Partial functions `expose_all`, `expose_get`, `expose_post`, `expose_put`, and `expose_delete`
+        are provided as convenience functions with pre-configured HTTP methods.
+    """
+
+    app = Flask(__name__)
+
+    @app.route(route, methods=methods)
+    def wrapper() -> Any:
+        return jsonify(func(json.loads(request.json)))
+
+    def run_server():
+        web_server = HTTPServer((hostname, port), app)
+        print("Server started http://%s:%s" % (hostname, port))
+        try:
+            web_server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+    threading.Thread(target=run_server).start()
+
+    return wrapper
+
+
+expose_all = functools.partial(expose, methods=['GET', 'POST', 'PUT', 'DELETE'])
+expose_get = functools.partial(expose, methods=['GET'])
+expose_post = functools.partial(expose, methods=['POST'])
+expose_put = functools.partial(expose, methods=['PUT'])
+expose_delete = functools.partial(expose, methods=['DELETE'])
